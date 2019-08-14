5b2c94ac57981d23c554494c4302f94d50639546
Justin <jmwhitney96@gmail.com>
2019-08-13 20:21:43 -0700

    Fixed an error in the perspective thumbnail script.
    Removed the handleMove event handler declaration from the inside of the for loop.
    Removed console logs.
    Changed the rotation angle based around the center of the target element, instead of the upper left corner.

--- scripts/perspectiveThumbnail/index.js
+++ scripts/perspectiveThumbnail/index.js
@@ -1,25 +1,19 @@
 (function imageThumbnails() {
   let thumbnails = document.getElementsByClassName("thumbnail");
-  console.log(thumbnails);
+  let currentElement = document;
+
+  function handleMove(moveEvent) {
+    let rect = currentElement.getBoundingClientRect(),
+        localMousePosX = moveEvent.clientX - rect.x,
+        localMousePosY = moveEvent.clientY - rect.y;
+
+    let yAngle = Math.floor(25 * ((localMousePosX - rect.width/2) / rect.width)) + 'deg';
+    let xAngle = Math.floor(25 * ((localMousePosY - rect.height/2) / rect.height)) + 'deg';
+        
+    currentElement.style.transform = `rotateX(${xAngle}) rotateY(${yAngle})`;
+  }
   
   for(let i = 0; i < thumbnails.length; i++) {
-
-    let currentElement = document;
-
-    function handleMove(moveEvent) {
-      moveEvent.stopPropagation();
-      let rect = currentElement.getBoundingClientRect(),
-          localMousePosX = moveEvent.clientX - rect.x,
-          localMousePosY = moveEvent.clientY - rect.y;
-
-      let xAngle = Math.floor(25 * (localMousePosX / rect.width)) + 'deg';
-      let yAngle = Math.floor(25 * (localMousePosX / rect.height)) + 'deg';
-      console.log(`rotateX(${xAngle}) rotateY(${yAngle})`);
-          
-      currentElement.style.transform = `rotateX(${xAngle}) rotateY(${yAngle})`;
-      console.log(currentElement.style.transform);
-    }
-
     (function(index) {
       thumbnails[i].onmouseover = (enterEvent) => {
         currentElement = enterEvent.currentTarget;
